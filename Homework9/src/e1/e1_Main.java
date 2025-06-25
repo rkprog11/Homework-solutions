22657c838c0152f79ad0050746e4bc9cacf3f14a
Robin Kraus <Robin Kraus@192.168.178.26>
2025-06-25 23:11:05 +0200

    E1 Main added.

--- Homework9/src/e1/e1_Main.java
+++ Homework9/src/e1/e1_Main.java
@@ -4,7 +4,12 @@ public class e1_Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		CoursePhase cp1 = CoursePhase.NOT_STARTED;
+		CoursePhase cp2 = CoursePhase.ONGOING;
+		CoursePhase cp3 = CoursePhase.FINISHED;
+		System.out.println(cp1.getMessage());
+		System.out.println(cp2.getMessage());
+		System.out.println(cp3.getMessage());
 	}
 
 }
